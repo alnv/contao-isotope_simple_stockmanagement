9f200469cf511ebd0faf7d6218f6c8c5372f04de
Alexander Naumov <alexandernaumov@AlexandGeschaft.fritz.box>
2024-07-15 14:52:21 +0200

    fx comp. to c413

--- src/Resources/contao/dca/tl_iso_product.php
+++ src/Resources/contao/dca/tl_iso_product.php
@@ -15,7 +15,7 @@ use Richardhj\IsotopeSimpleStockManagement\BackendIntegration\LabelCallbackListe
 
 $table = Isotope\Model\Product::getTable();
 
-$GLOBALS['TL_DCA'][$table]['list']['label']['fields'][] = 'stock';
+// $GLOBALS['TL_DCA'][$table]['list']['label']['fields'][] = 'stock';
 
 $GLOBALS['TL_DCA'][$table]['list']['label']['label_callback.default'] = $GLOBALS['TL_DCA'][$table]['list']['label']['label_callback'];
 $GLOBALS['TL_DCA'][$table]['list']['label']['label_callback'] = [LabelCallbackListener::class, 'generate'];
